dc5e9d1877d3c3a8cf534be9b292faf4800d3b84
SajeenaMahotra <149761433+SajeenaMahotra@users.noreply.github.com>
2025-12-04 22:30:24 +0545

    add login screen and snackbar

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
+import 'package:sewa_hub/commons/snackbar.dart';
 import 'package:sewa_hub/widget/button2.dart';
 import 'package:sewa_hub/widget/custom_text_field.dart';
 
@@ -13,40 +16,98 @@ class _LoginScreenState extends State<LoginScreen> {
   final _formKey = GlobalKey<FormState>();
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController= TextEditingController();
+  
   @override
+  void dispose(){
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  void _validateAndLogin() {
+    if (_formKey.currentState!.validate()) {
+      // Login successful
+      showSnackbar(
+        context: context,
+        message: "Login Successful!",
+        color: Colors.green,
+      );
+      // Clear fields after successful login
+      _emailController.clear();
+      _passwordController.clear();
+    } else {
+      // Login failed
+      showSnackbar(
+        context: context,
+        message: "Please fix the errors",
+        color: Colors.red,
+      );
+    }
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Column(
-        children: [
-          SizedBox(height: 150,),
-          SizedBox(
-            height: 60,
-            width: 300,
-            child: Image.asset('assets/images/sewahub_logo.png',fit:BoxFit.contain)),
-            SizedBox(height: 20,),
-            Text("Welcome Back!", style: TextStyle(fontSize: 30,fontWeight: FontWeight.bold),),
-            SizedBox(height: 5,),
-            Text("Your Trusted Services Await",style: TextStyle(fontSize: 20),),
-            SizedBox(height: 30,),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: CustomTextField(controller: _emailController,
-              labelText: "Email",
-              errorText: "Please enter your email address",
-              hintText: 'johndoe@gmail.com',),
-            ),
-            SizedBox(height: 10,),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: CustomTextField(controller: _passwordController,
-              labelText: "Password",
-              errorText: "Please enter your password",
-              hintText: '********',),
-            ),
-            SizedBox(height: 20,),
-            Button2(text: "Login", onPressed: () {},),
-            ],
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(height: 150,),
+            SizedBox(
+              height: 60,
+              width: 300,
+              child: Image.asset('assets/images/sewahub_logo.png',fit:BoxFit.contain)),
+              SizedBox(height: 20,),
+              Text("Welcome Back!", style: TextStyle(fontSize: 30,fontWeight: FontWeight.bold),),
+              SizedBox(height: 5,),
+              Text("Your Trusted Services Await",style: TextStyle(fontSize: 20),),
+              SizedBox(height: 30,),
+              Form(
+                key: _formKey,
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: CustomTextField(controller: _emailController,
+                      labelText: "Email",
+                      errorText: "Please enter your email address",
+                      hintText: 'johndoe@gmail.com',
+                      obscureText: false,
+                      validator: (value){
+                        if (value == null || value.isEmpty){
+                          return "Email is required";
+                        }
+                        if ( !RegExp(r'^[^@]+@[^@]+\.[^@]+').hasMatch(value)){
+                          return "Please enter a valid email";
+                        }
+                        return null;
+                      },),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 10,),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: CustomTextField(controller: _passwordController,
+                labelText: "Password",
+                errorText: "Please enter your password",
+                hintText: '********',
+                obscureText: true,
+                validator: (value){
+                  if(value == null || value.isEmpty){
+                    return "Password is required";
+                  }
+                  if(value.length < 6){
+                    return "Password must be at least 6 characters";
+                  }
+                  return null;
+                },
+                ),
+              ),
+              SizedBox(height: 20,),
+              Button2(text: "Login", onPressed: () {},),
+              ],
+        ),
       ),
     );
   }
--- lib/widget/custom_text_field.dart
+++ lib/widget/custom_text_field.dart
@@ -1,37 +1,66 @@
 import 'package:flutter/material.dart';
 
 class CustomTextField extends StatefulWidget {
-  const CustomTextField({super.key,
-  required this.controller,
-  required this.errorText,
-  required this.labelText,
-  required this.hintText});
+  const CustomTextField({
+    super.key,
+    required this.controller,
+    required this.errorText,
+    required this.labelText,
+    required this.hintText,
+    required this.obscureText,
+    this.validator,
+  });
 
   final TextEditingController controller;
   final String labelText;
   final String hintText;
   final String errorText;
+  final bool obscureText;
+  final String? Function(String?)? validator;
 
   @override
   State<CustomTextField> createState() => _CustomTextFieldState();
 }
 
 class _CustomTextFieldState extends State<CustomTextField> {
+  late bool _obscureText;
   @override
+
+  void initState(){
+    super.initState();
+    _obscureText=widget.obscureText;
+  }
+
   Widget build(BuildContext context) {
     return TextFormField(
+      controller: widget.controller,
+      obscureText: _obscureText,
+      validator: widget.validator,
       decoration: InputDecoration(
         labelText: widget.labelText,
         hintText: widget.hintText,
         enabledBorder: OutlineInputBorder(
-          borderRadius:BorderRadius.circular(15),
+          borderRadius: BorderRadius.circular(15),
           borderSide: BorderSide(color: Colors.black),
         ),
         focusedBorder: OutlineInputBorder(
           borderRadius: BorderRadius.circular(15),
-          borderSide: BorderSide(color: Color(0xFFFF7940))        )
+          borderSide: BorderSide(color: Color(0xFFFF7940)),
+        ),
+        suffixIcon: widget.obscureText
+            ? IconButton(
+                icon: Icon(
+                  _obscureText ? Icons.visibility_off : Icons.visibility,
+                  color: const Color(0xFFFF7940),
+                ),
+                onPressed: () {
+                  setState(() {
+                    _obscureText = !_obscureText;
+                  });
+                },
+              )
+            : null,
       ),
-      controller: widget.controller,
     );
   }
 }
