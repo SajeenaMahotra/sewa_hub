ffe4d4d1a1d95cda5bc112a4af4d42dfee7d7cc3
SajeenaMahotra <149761433+SajeenaMahotra@users.noreply.github.com>
2025-12-14 10:31:57 +0545

    Add fonts

---
+++ lib/theme/theme_data.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+ThemeData getApplicationTheme(){
+  return ThemeData(
+    colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
+    fontFamily: 'Montserrat Regular'
+  );
+}
