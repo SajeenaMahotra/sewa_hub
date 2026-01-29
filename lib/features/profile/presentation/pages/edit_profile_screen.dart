cdc5c2ad897e84f60731a6bbad8c4ba0dbe8978a
SajeenaMahotra <149761433+SajeenaMahotra@users.noreply.github.com>
2026-01-29 21:39:10 +0545

    add camera and gallery permission

--- lib/features/profile/presentation/pages/edit_profile_screen.dart
+++ lib/features/profile/presentation/pages/edit_profile_screen.dart
@@ -1,8 +1,8 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:sewa_hub/core/utils/snackbar_utils.dart';
 import 'package:sewa_hub/core/widgets/button2.dart';
 import 'package:sewa_hub/core/widgets/button_outline.dart';
 import 'package:sewa_hub/core/widgets/custom_text_field.dart';
@@ -32,8 +32,98 @@ class _EditProfilePageState extends ConsumerState<EditProfilePage> {
     super.dispose();
   }
 
-  final List<XFile>_selectedMedia = [];
+  final List<XFile> _selectedMedia = []; //images.video
+  final ImagePicker _imagePicker = ImagePicker();
 
+  Future<bool> _askUserPermission(Permission permission) async {
+    final status = await permission.status;
+    if (status.isGranted) {
+      return true;
+    }
+
+    if (status.isDenied) {
+      final result = await permission.request();
+      return result.isGranted;
+    }
+
+    if (status.isPermanentlyDenied) {
+      _showPermissionDeniedDialog();
+      return false;
+    }
+    return false;
+  }
+
+  //code for camera
+  Future<void> _cameraPermission() async {
+    final hasPermission = await _askUserPermission(Permission.camera);
+    if (!hasPermission) return;
+
+    final XFile? photo = await _imagePicker.pickImage(
+      source: ImageSource.camera,
+      imageQuality: 80,
+    );
+
+    if (photo != null) {
+      setState(() {
+        _selectedMedia.clear();
+        _selectedMedia.add(photo);
+      });
+    }
+  }
+
+  //code for gallery
+  Future<void> _galleryPermission({bool allowMultiple = false}) async {
+    try {
+      if (allowMultiple) {
+        final List<XFile>? images = await _imagePicker.pickMultiImage(
+          imageQuality: 80,
+        );
+
+        if (images != null && images.isNotEmpty) {
+          setState(() {
+            _selectedMedia.clear();
+            _selectedMedia.addAll(images);
+          });
+        }
+      } else {
+        final XFile? image = await _imagePicker.pickImage(
+          source: ImageSource.gallery,
+          imageQuality: 80,
+        );
+
+        if (image != null) {
+          setState(() {
+            _selectedMedia.clear();
+            _selectedMedia.add(image);
+          });
+        }
+      }
+    } catch (e) {
+      debugPrint("Error picking image: $e");
+
+      if (!mounted) {
+        SnackbarUtils.showError(
+          context,
+          message: "Unable to pick image from the gallery",
+        );
+      }
+      ;
+    }
+  }
+
+  void _showPermissionDeniedDialog() {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text("Permission required"),
+        content: Text("Go to settings to grant permission"),
+        actions: [
+          TextButton(onPressed: () {}, child: Text('Cancel')),
+          TextButton(onPressed: () {}, child: Text('Open Settings')),
+        ],
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -62,16 +152,21 @@ class _EditProfilePageState extends ConsumerState<EditProfilePage> {
                   Positioned(
                     bottom: 0,
                     right: 0,
-                    child: Container(
-                      decoration: BoxDecoration(
-                        color: Colors.orange,
-                        shape: BoxShape.circle,
-                      ),
-                      padding: const EdgeInsets.all(8),
-                      child: const Icon(
-                        Icons.edit,
-                        color: Colors.white,
-                        size: 20,
+                    child: GestureDetector(
+                      onTap: () async {
+                        await _cameraPermission();
+                      },
+                      child: Container(
+                        decoration: const BoxDecoration(
+                          color: Colors.orange,
+                          shape: BoxShape.circle,
+                        ),
+                        padding: const EdgeInsets.all(8),
+                        child: const Icon(
+                          Icons.edit,
+                          color: Colors.white,
+                          size: 20,
+                        ),
                       ),
                     ),
                   ),
@@ -85,7 +180,7 @@ class _EditProfilePageState extends ConsumerState<EditProfilePage> {
               style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
             ),
             const SizedBox(height: 20),
-            
+
             CustomTextField(
               controller: fullNameController,
               labelText: "Full Name",
@@ -94,7 +189,7 @@ class _EditProfilePageState extends ConsumerState<EditProfilePage> {
               obscureText: false,
             ),
             const SizedBox(height: 20),
-      
+
             CustomTextField(
               controller: emailController,
               labelText: "Email",
@@ -103,20 +198,14 @@ class _EditProfilePageState extends ConsumerState<EditProfilePage> {
               obscureText: false,
             ),
             const SizedBox(height: 30),
-    
-            Button2(
-              text: "Update Profile",
-              fontSize: 20,
-              onPressed: () {
-                
-              },
-            ),
+
+            Button2(text: "Update Profile", fontSize: 20, onPressed: () {}),
             const SizedBox(height: 15),
-        
+
             ButtonOutline(
               text: "Change Password",
               onPressed: () {},
-              color: Colors.red, 
+              color: Colors.red,
             ),
           ],
         ),
